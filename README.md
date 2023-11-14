08236956f51eda2eb9384fc3194310c1d7f1f08e
dmitry-koverko <63540108+dmitry-koverko@users.noreply.github.com>
2023-11-14 20:34:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AgraKMM
+ 
---
+++ shared/src/iosTest/kotlin/com/example/agrakmm/iosTest.kt
@@ -0,0 +1,12 @@
+package com.example.agrakmm
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class IosGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greet().contains("iOS"), "Check iOS is mentioned")
+    }
+}
